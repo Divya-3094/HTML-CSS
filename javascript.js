9fb782969d4b5fbc7ae4cc6273ec6caf79791301
Divya-3094 <divyavallabhapuramd1@gmail.com>
2025-11-03 19:38:49 +0530

    version

---
+++ javascript.js
@@ -0,0 +1,18 @@
+id=prompt("Enter Employe ID:")
+document.write("Employe ID:"+id+"<br>");
+ename=prompt('Enter Employee name: ')
+document.write('Employe Name: '+ename+"<br>");
+email=prompt('Enter Employe Email: ')
+document.write('Employe Email: '+email+"<br>");
+phoneno=prompt('Enter Employe Phone Number: ')
+document.write('Employe Mobile No: '+phoneno+"<br>");
+salary=prompt('Enter Employe salary: ')
+document.write('Enter Employe Salary: '+salary+"<br>");
+experience=prompt('Enter Employe Experience: ')
+document.write('Employe year of Experience: '+experience+"<br>");
+joiningdate=prompt('Enter Employe Date of Joining: ')
+document.write('Employe Date of Joining: '+joiningdate+"<br>");
+place=prompt('Enter Employe Location: ')
+document.write('Employe Location: '+place+"<br>");
+designation=prompt('Enter Employe Designation: ')
+document.write('Employe Designation: '+designation+"<br>")
